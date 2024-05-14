00232c5f115f8be4deaba45911a9353a7bdf56c4
sidane17 <khalilsidane89@gmail.com>
2024-05-14 16:12:25 +0200

    lista

--- main.js
+++ main.js
@@ -4,12 +4,18 @@ createApp({
     data(){
         return{
             compito : '',
-            Todo:["uno","due","tre"]
+            Todo:[],
+            show:false
         }
     },
     methods:{
         addLista(){
-            
+            if(this.compito!=''){
+                this.Todo.push(this.compito)
+            }
+        },
+        remove(i){
+            delete this.Todo[i]
         }
     }
 }).mount('#app');
